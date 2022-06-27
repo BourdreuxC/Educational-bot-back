15e80e29a8ca7dcd6bac0f710ea10f1800173cd3
bourdreux charlelie <charlelie.bourdreux@diiage.org>
2022-06-27 11:44:04 +0200

    interface implementation

--- bot-api/EducationalTeamsBotApi/EducationalTeamsBotApi.UnitTests/MockServices/TagsMockService.cs
+++ bot-api/EducationalTeamsBotApi/EducationalTeamsBotApi.UnitTests/MockServices/TagsMockService.cs
@@ -59,5 +59,10 @@ namespace EducationalTeamsBotApi.UnitTests.MockServices
         {
             throw new NotImplementedException();
         }
+
+        public Task<CosmosTag?> AddTag(string id, List<string> variants)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
